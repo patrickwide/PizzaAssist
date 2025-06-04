435b49709a7ea8aabd60cd779cd2f9d09f080e2e
patrickwide <patrickwide254@gmail.com>
2025-06-04 19:25:40 +0300

    resolve the path duplication issue and allow the application to find the store metadata file at the correct location

--- core/constants.py
+++ core/constants.py
@@ -5,7 +5,7 @@ HISTORY_DIR = f"{DATA_DIR}/history"  # History and conversation files
 REVIEWS_DIR = f"{DATA_DIR}/reviews"  # Reviews and orders
 
 # --- Path Configuration ---
-DB_LOCATION = f"./{DB_DIR}"  # Directory for Chroma DB
+DB_LOCATION = DB_DIR  # Directory for Chroma DB
 CONVERSATION_HISTORY_FILE_PATH = f"{HISTORY_DIR}/conversation_history.jsonl"  # File to save conversation history
 CSV_FILE_PATH = f"{REVIEWS_DIR}/realistic_restaurant_reviews.csv"  # Reviews file
 ORDER_FILE_PATH = f"{REVIEWS_DIR}/orders.txt"  # File to save orders
